80910fb61d9f0cb186c136184281266d2407533a
Ajay Singh <meajaysingh@hotmail.com>
2016-08-12 14:21:25 +0530

    App configuration

---
+++ src/app/config.service.ts
@@ -0,0 +1,8 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCJHSlFe7VQ_-x1n00fxrn1-WKbPmoJcbw",
+  authDomain: "coderajayio.firebaseapp.com",
+  databaseURL: "https://coderajayio.firebaseio.com",
+  storageBucket: "coderajayio.appspot.com",
+}; 
