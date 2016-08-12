31a82dbf836ed14541c80fb496213522b8acee2a
Ajay Singh <meajaysingh@hotmail.com>
2016-08-12 12:23:57 +0530

    using shared module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }           from '@angular/core';
 
-import { HomeComponent }   from './home.component';
+import { SharedModule }       from '../shared';
+import { HomeComponent }      from './home.component';
 import { routing }            from './home.routing';
 @NgModule({
-  imports:      [ routing ],
-  declarations: [ HomeComponent ],
-  providers:    [  ]
+  imports: [
+    SharedModule,
+    routing
+  ],
+  declarations: [HomeComponent],
+  providers: []
 })
 export class HomeModule { }
